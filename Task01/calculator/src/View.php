49f1d6521a7c762a86bd6dbcc99c35ba8dab868c
ZerolLka <masterdew4@gmail.com>
2026-02-26 00:35:13 +0300

    Update View.php

--- Task01/calculator/src/View.php
+++ Task01/calculator/src/View.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace ZerolLka\Calculator\View;
 
 use function cli\line;
 
-function showWelcome(): void
-{
-    line("Добро пожаловать в игру Калькулятор!");
-    line("Вычислите значение выражения.");
+function showMessage(string $msg): void {
+    line($msg);
 }
